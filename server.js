87ec978a77ebe40e20288e2459f4c7d8600facf5
urigross <urigross@gmail.com>
2022-09-07 11:15:51 +0300

    rewrite app.listen at server.js

--- server.js
+++ server.js
@@ -23,4 +23,6 @@ if (process.env.NODE_ENV !== 'production') {
   
   app.use('/', indexRouter)
   
-  app.listen(process.env.PORT || 3000)
+ app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
